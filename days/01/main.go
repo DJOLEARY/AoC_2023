39a2607719663a8e1a3f919c9a6f9f27fe4aedd9
DJ O'Leary <djoleary39@ymail.com>
2023-12-01 19:06:06 +0100

    Update day 01 code to calculate pt 2

--- days/01/main.go
+++ days/01/main.go
@@ -39,27 +39,56 @@ func read_file(filename string) string {
 	return string(content)
 }
 
+var num_map = map[string]string{
+	"zero":  "0",
+	"0":     "0",
+	"one":   "1",
+	"1":     "1",
+	"two":   "2",
+	"2":     "2",
+	"three": "3",
+	"3":     "3",
+	"four":  "4",
+	"4":     "4",
+	"five":  "5",
+	"5":     "5",
+	"six":   "6",
+	"6":     "6",
+	"seven": "7",
+	"7":     "7",
+	"eight": "8",
+	"8":     "8",
+	"nine":  "9",
+	"9":     "9",
+}
+
 func find_numbers(line string) (string, string, error) {
 	if line == "" || line == "\n" {
 		return "", "", errors.New("Empty line")
 	}
 
-	first := ""
-	last := ""
-
-	chars := strings.Split(line, "")
-	for _, char := range chars {
-		is_numeric := strings.Contains("0123456789", char)
-		if !is_numeric {
-			continue
+	found_numbers := []string{}
+	window_size := 5
+	for i := range line {
+		window_end := i + window_size
+		if window_end > len(line)-1 {
+			window_end = len(line) - 1
 		}
+		sub_str := line[i:window_end]
 
-		if first == "" {
-			first = char
+		for key, value := range num_map {
+			if strings.HasPrefix(sub_str, key) {
+				found_numbers = append(found_numbers, value)
+			}
 		}
-
-		last = char
 	}
+
+	if len(found_numbers) == 0 {
+		return "", "", errors.New("No numbers found")
+	}
+
+	first := found_numbers[0]
+	last := found_numbers[len(found_numbers)-1]
 
 	return first, last, nil
 }
