98b1837aecf396c6d2eeab57582d787fc35b8db5
DJ O'Leary <djoleary39@ymail.com>
2024-07-11 00:42:56 +0200

    feat: update path to files in script

--- scripts/generate_and_open_coverage_report.sh
+++ scripts/generate_and_open_coverage_report.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # author: dijitol
+# description: Generate and open coverage report in browser. Should be run from the root of the project
 
-go test -coverprofile=coverage.out ../... 
+go test -coverprofile=coverage.out ./... 
 go tool cover -html=coverage.out
