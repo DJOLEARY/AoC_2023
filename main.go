101a8dbe4d32b1c27541d1748e4b71059c8380d3
DJ O'Leary <djoleary39@ymail.com>
2024-07-10 21:48:45 +0200

    feat: Use CLI flags --day and --part to run packages

--- main.go
+++ main.go
@@ -1,9 +1,85 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/DJOLEARY/AoC_2023/internal/day_01"
+	day_02_part_01 "github.com/DJOLEARY/AoC_2023/internal/day_02/part_01"
+	day_02_part_02 "github.com/DJOLEARY/AoC_2023/internal/day_02/part_02"
+	day_03_part_01 "github.com/DJOLEARY/AoC_2023/internal/day_03/part_01"
+	day_04_part_01 "github.com/DJOLEARY/AoC_2023/internal/day_04/part_01"
+	day_04_part_02 "github.com/DJOLEARY/AoC_2023/internal/day_04/part_02"
+	day_06_part_01 "github.com/DJOLEARY/AoC_2023/internal/day_06/part_01"
+	day_06_part_02 "github.com/DJOLEARY/AoC_2023/internal/day_06/part_02"
+	day_07_part_01 "github.com/DJOLEARY/AoC_2023/internal/day_07/part_01"
 )
 
 func main() {
-	day_01.Solve()
+	day := flag.Int("day", 1, "Day to run")
+	part := flag.Int("part", 2, "Part to run, must be 1 or 2")
+
+	flag.Parse()
+
+	if *part < 1 || *part > 2 {
+		println("Invalid part number, must be 1 or 2")
+		os.Exit(1)
+	}
+
+	switch *day {
+	case 1:
+		switch *part {
+		case 1:
+			println("Day 1 part 1 not implemented")
+			os.Exit(1)
+		case 2:
+			day_01.Solve()
+		}
+
+	case 2:
+		switch *part {
+		case 1:
+			day_02_part_01.Solve()
+		case 2:
+			day_02_part_02.Solve()
+		}
+
+	case 3:
+		switch *part {
+		case 1:
+			day_03_part_01.Solve()
+		case 2:
+			println("Day 3 part 2 not implemented")
+			os.Exit(1)
+		}
+
+	case 4:
+		switch *part {
+		case 1:
+			day_04_part_01.Solve()
+		case 2:
+			day_04_part_02.Solve()
+		}
+
+	case 6:
+		switch *part {
+		case 1:
+			day_06_part_01.Solve()
+		case 2:
+			day_06_part_02.Solve()
+		}
+
+	case 7:
+		switch *part {
+		case 1:
+			day_07_part_01.Solve()
+		case 2:
+			println("Day 7 part 2 not implemented")
+			os.Exit(1)
+		}
+
+	default:
+		println("Day not implemented")
+		os.Exit(1)
+	}
 }
